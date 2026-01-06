5964b715b676f4d7adf1e7df8c95d7123e2a446a
Rick Love <rick@toldpro.com>
2026-01-05 18:01:03 -0600

    wip node store

--- packages/workbench-app/src/workflow/store/create-react-flow-store.ts
+++ packages/workbench-app/src/workflow/store/create-react-flow-store.ts
@@ -1,5 +1,212 @@
-import type { WorkflowReactFlowStore, WorkflowRuntimeStore } from "../types";
+/* eslint-disable react-hooks/immutability */
+import { proxy, snapshot, subscribe, useSnapshot } from 'valtio';
+import {
+  WorkflowBrandedTypes,
+  type WorkflowReactFlowStore,
+  type WorkflowRuntimeEdge,
+  type WorkflowRuntimeNode,
+  type WorkflowRuntimeStore,
+} from '../types';
+import { type EdgeChange, type NodeChange } from '@xyflow/react';
+import { useEffect, useState } from 'react';
+import { memoize } from 'proxy-memoize';
 
 export const createReactFlowStore = (store: WorkflowRuntimeStore): WorkflowReactFlowStore => {
-    // TODO: implement
+  type StoreSnap = ReturnType<typeof snapshot<typeof store>>;
+
+  const getNodeTypes = memoize((store: StoreSnap) =>
+    Object.fromEntries(
+      Object.values(store.nodeTypes).map((x) => [x.type, x.component as React.ComponentType]),
+    ),
+  );
+
+  const getNode = memoize((x: WorkflowRuntimeNode) => {
+    const nodeDirect = store.nodes[x.id]!;
+    const pos = x.position;
+    return {
+      id: x.id,
+      type: x.type,
+      position: { x: pos.x, y: pos.y },
+      width: pos.width,
+      height: pos.height,
+
+      parentId: x.parentId,
+      data: { node: nodeDirect },
+    };
+  });
+  const getNodes = memoize((snap: StoreSnap) =>
+    Object.values(snap.nodes as unknown as WorkflowRuntimeNode[]).map((x) => getNode(x)),
+  );
+
+  const getEdge = memoize((x: WorkflowRuntimeEdge) =>
+    x.source.error
+      ? undefined
+      : {
+          id: x.id,
+          type: `custom` as const,
+          source: x.source.node.id,
+          sourceHandle: x.source.outputName,
+          target: x.target.node.id,
+          targetHandle: x.target.inputName,
+          data: { edge: store.edges[x.id]! as WorkflowRuntimeEdge },
+        },
+  );
+  const getEdges = memoize((snap: StoreSnap) =>
+    Object.values(snap.edges as unknown as WorkflowRuntimeEdge[])
+      .map((x) => getEdge(x))
+      .filter((x): x is NonNullable<typeof x> => !!x),
+  );
+
+  const reactStore: WorkflowReactFlowStore & {
+    _store: WorkflowRuntimeStore;
+  } = {
+    _store: store,
+    get nodeTypes() {
+      return getNodeTypes(snapshot(this._store));
+    },
+    get nodes() {
+      return getNodes(snapshot(this._store));
+    },
+    get edges() {
+      return getEdges(snapshot(this._store));
+    },
+  };
+
+  return proxy(reactStore);
 };
+
+export const useReactFlowStore = (
+  store: WorkflowRuntimeStore,
+): WorkflowReactFlowStore & {
+  onNodesChange: (changes: NodeChange[]) => void;
+  onEdgesChange: (changes: EdgeChange[]) => void;
+} => {
+  const [nodeTypes, setNodeTypes] = useState({} as WorkflowReactFlowStore['nodeTypes']);
+  useEffect(() => {
+    const unsub = subscribe(store.nodeTypes, () => {
+      setNodeTypes(
+        Object.fromEntries(
+          Object.values(store.nodeTypes).map((x) => [x.type, x.component as React.ComponentType]),
+        ),
+      );
+    });
+    return () => unsub();
+  }, [store]);
+
+  const snap = useSnapshot(store) as unknown as typeof store;
+
+  const reactFlowStore: WorkflowReactFlowStore = {
+    nodeTypes,
+    nodes: Object.values(snap.nodes).map((x) => {
+      const nodeDirect = store.nodes[x.id]!;
+      const pos = x.position;
+      return {
+        id: x.id,
+        type: x.type,
+        position: { x: pos.x, y: pos.y },
+        width: pos.width,
+        height: pos.height,
+
+        parentId: x.parentId,
+        data: { node: nodeDirect },
+      };
+    }),
+    edges: Object.values(snap.edges)
+      .map((x) =>
+        x.source.error
+          ? undefined
+          : {
+              id: x.id,
+              type: `custom` as const,
+              source: x.source.node.id,
+              sourceHandle: x.source.outputName,
+              target: x.target.node.id,
+              targetHandle: x.target.inputName,
+              data: { edge: store.edges[x.id]! as WorkflowRuntimeEdge },
+            },
+      )
+      .filter((x): x is NonNullable<typeof x> => !!x),
+  };
+
+  return {
+    ...reactFlowStore,
+    onNodesChange: (changes: NodeChange[]) => {
+      for (const change of changes) {
+        if (change.type === 'position' || change.type === 'dimensions') {
+          const node = store.nodes[WorkflowBrandedTypes.nodeId(change.id)];
+          if (!node) continue;
+
+          if (change.type === `position`) {
+            node.position.x = change.position?.x ?? node.position.x;
+            node.position.y = change.position?.y ?? node.position.y;
+          }
+          if (change.type === `dimensions`) {
+            node.position.width = change.dimensions?.width ?? node.position.width;
+            node.position.height = change.dimensions?.height ?? node.position.height;
+          }
+          continue;
+        }
+
+        if (change.type === `remove`) {
+          store.actions.deleteNode(WorkflowBrandedTypes.nodeId(change.id));
+          continue;
+        }
+
+        console.log(`[useReactFlowStore] Unhandled node change: `, { change });
+      }
+    },
+    onEdgesChange: (changes: EdgeChange[]) => {
+      for (const change of changes) {
+        if (change.type === `remove`) {
+          store.actions.deleteEdge(WorkflowBrandedTypes.edgeIdFormString(change.id));
+          continue;
+        }
+
+        console.log(`[useReactFlowStore:onEdgesChange] Unhandled edge change: `, { change });
+      }
+    },
+  };
+};
+
+// import {
+//   Edge,
+//   EdgeChange,
+//   Node,
+//   NodeChange,
+//   OnNodesChange,
+//   OnEdgesChange,
+//   applyNodeChanges,
+//   applyEdgeChanges,
+// } from '@xyflow/react';
+// import { createWithEqualityFn } from 'zustand/traditional';
+
+// export type RFState = {
+//   nodes: Node[];
+//   edges: Edge[];
+//   onNodesChange: OnNodesChange;
+//   onEdgesChange: OnEdgesChange;
+// };
+
+// const useStore = createWithEqualityFn<RFState>((set, get) => ({
+//   nodes: [
+//     {
+//       id: 'root',
+//       type: 'mindmap',
+//       data: { label: 'React Flow Mind Map' },
+//       position: { x: 0, y: 0 },
+//     },
+//   ],
+//   edges: [],
+//   onNodesChange: (changes: NodeChange[]) => {
+//     set({
+//       nodes: applyNodeChanges(changes, get().nodes),
+//     });
+//   },
+//   onEdgesChange: (changes: EdgeChange[]) => {
+//     set({
+//       edges: applyEdgeChanges(changes, get().edges),
+//     });
+//   },
+// }));
+
+// export default useStore;
--- packages/workbench-app/src/workflow/types.ts
+++ packages/workbench-app/src/workflow/types.ts
@@ -2,242 +2,259 @@ type JsonValue = string | number | boolean | null | JsonObject | JsonArray;
 type JsonObject = { [key: string]: JsonValue };
 type JsonArray = JsonValue[];
 
-type WorkflowNodeTypeName = string & { __brand: "WorkflowNodeTypeName" };
-type WorkflowValueType = string & { __brand: "WorkflowValueType" };
-type WorkflowTimestamp = number & { __brand: "WorkflowTimestamp", __kind: `performance.timeOrigin+performance.now()` };
+type WorkflowNodeTypeName = string & { __brand: 'WorkflowNodeTypeName' };
+type WorkflowValueType = string & { __brand: 'WorkflowValueType' };
+type WorkflowTimestamp = number & {
+  __brand: 'WorkflowTimestamp';
+  __kind: `performance.timeOrigin+performance.now()`;
+};
 
-type WorkflowNodeId = string & { __brand: "WorkflowNodeId" };
-type WorkflowEdgeId = string & { __brand: "WorkflowEdgeId" };
-type WorkflowOutputName = string & { __brand: "WorkflowOutputName" };
-type WorkflowInputName = string & { __brand: "WorkflowInputName" };
+type WorkflowNodeId = string & { __brand: 'WorkflowNodeId' };
+type WorkflowEdgeId = string & { __brand: 'WorkflowEdgeId' };
+type WorkflowOutputName = string & { __brand: 'WorkflowOutputName' };
+type WorkflowInputName = string & { __brand: 'WorkflowInputName' };
 
 export const WorkflowBrandedTypes = {
-    typeName: (value: string) => value as unknown as WorkflowNodeTypeName,
-    T: (strings: TemplateStringsArray) => strings[0] as unknown as WorkflowNodeTypeName,
-    valueType: (value: string) => value as unknown as WorkflowValueType,
-    V: (strings: TemplateStringsArray) => strings[0] as unknown as WorkflowValueType,
-    inputName: (value: string) => value as unknown as WorkflowInputName,
-    I: (strings: TemplateStringsArray) => strings[0] as unknown as WorkflowInputName,
-    outputName: (value: string) => value as unknown as WorkflowOutputName,
-    O: (strings: TemplateStringsArray) => strings[0] as unknown as WorkflowOutputName,
-    nodeId: (value: string) => value as unknown as WorkflowNodeId,
-    N: (strings: TemplateStringsArray) => strings[0] as unknown as WorkflowNodeId,
-    edgeId: (sourceNodeId: string, sourceOutputName: string, targetNodeId: string, targetInputName: string): WorkflowEdgeId => {
-        return `${sourceNodeId}:${sourceOutputName}=>${targetNodeId}:${targetInputName}` as unknown as WorkflowEdgeId;
-    },
+  typeName: (value: string) => value as unknown as WorkflowNodeTypeName,
+  T: (strings: TemplateStringsArray) => strings[0] as unknown as WorkflowNodeTypeName,
+  valueType: (value: string) => value as unknown as WorkflowValueType,
+  V: (strings: TemplateStringsArray) => strings[0] as unknown as WorkflowValueType,
+  inputName: (value: string) => value as unknown as WorkflowInputName,
+  I: (strings: TemplateStringsArray) => strings[0] as unknown as WorkflowInputName,
+  outputName: (value: string) => value as unknown as WorkflowOutputName,
+  O: (strings: TemplateStringsArray) => strings[0] as unknown as WorkflowOutputName,
+  nodeId: (value: string) => value as unknown as WorkflowNodeId,
+  N: (strings: TemplateStringsArray) => strings[0] as unknown as WorkflowNodeId,
+  edgeIdFormString: (value: string) => value as unknown as WorkflowEdgeId,
+  edgeId: (
+    sourceNodeId: string,
+    sourceOutputName: string,
+    targetNodeId: string,
+    targetInputName: string,
+  ): WorkflowEdgeId => {
+    return `${sourceNodeId}:${sourceOutputName}=>${targetNodeId}:${targetInputName}` as unknown as WorkflowEdgeId;
+  },
 
-    now: () => performance.timeOrigin + performance.now() as unknown as WorkflowTimestamp,
-
+  now: () => (performance.timeOrigin + performance.now()) as unknown as WorkflowTimestamp,
 };
 
 export type WorkflowDocumentData = {
-    nodes: {
-        id: WorkflowNodeId;
-        type: WorkflowNodeTypeName;
-        parentId?: WorkflowNodeId;
-        position: {
-            x: number;
-            y: number,
-            width: number;
-            height: number;
-        };
-        inputs: {
-            name: WorkflowInputName;
-            type: WorkflowValueType;
-            source?: {
-                nodeId: WorkflowNodeId;
-                name: WorkflowOutputName;
-            };
-        }[];
-        outputs: {
-            name: WorkflowOutputName;
-            type: WorkflowValueType;
-        }[];
-        data?: JsonObject;
-        mode?: `passthrough` | `disabled`;
-    }[];
-};
-
-export type WorkflowReactFlowStore = {
-    nodeTypes: Record<WorkflowNodeTypeName, React.ComponentType>;
-    nodes: {
-        id: WorkflowNodeId;
-        type: WorkflowNodeTypeName;
-        position: { x: number; y: number };
-        width: number;
-        height: number;
-        parentId?: WorkflowNodeId;
-        extent?: 'parent',
-        data: {
-            node: WorkflowRuntimeNode;
-        };
-    }[];
-    edges: {
-        id: WorkflowEdgeId;
-        type: `custom`;
-        source: WorkflowNodeId;
-        sourceHandle: WorkflowOutputName;
-        target: WorkflowNodeId;
-        targetHandle: WorkflowInputName;
-        data: {
-            edge: WorkflowRuntimeEdge;
-        };
-    }[];
-};
-
-export type WorkflowRuntimeNode = {
+  nodes: {
     id: WorkflowNodeId;
     type: WorkflowNodeTypeName;
     parentId?: WorkflowNodeId;
     position: {
-        x: number;
-        y: number,
-        width: number;
-        height: number;
-        extent?: 'parent',
+      x: number;
+      y: number;
+      width: number;
+      height: number;
     };
     inputs: {
-        name: WorkflowInputName;
-        type: WorkflowValueType;
-        value?: WorkflowRuntimeValue;
-        edge?: WorkflowRuntimeEdge;
+      name: WorkflowInputName;
+      type: WorkflowValueType;
+      source?: {
+        nodeId: WorkflowNodeId;
+        name: WorkflowOutputName;
+      };
     }[];
     outputs: {
-        name: WorkflowOutputName;
-        type: WorkflowValueType;
-        value?: WorkflowRuntimeValue;
-        edges?: WorkflowRuntimeEdge[];
+      name: WorkflowOutputName;
+      type: WorkflowValueType;
     }[];
-    data: JsonObject;
+    data?: JsonObject;
     mode?: `passthrough` | `disabled`;
-    executionState?: WorkflowRuntimeExecutionState;
-    graphErrors?: {
-        kind: `missing-type-definition`;
-    }[];
+  }[];
+};
+
+export type WorkflowReactFlowStore = {
+  nodeTypes: Record<WorkflowNodeTypeName, React.ComponentType>;
+  nodes: {
+    id: WorkflowNodeId;
+    type: WorkflowNodeTypeName;
+    position: { x: number; y: number };
+    width: number;
+    height: number;
+    parentId?: WorkflowNodeId;
+    extent?: 'parent';
+    data: {
+      node: WorkflowRuntimeNode;
+    };
+  }[];
+  edges: {
+    id: WorkflowEdgeId;
+    type: `custom`;
+    source: WorkflowNodeId;
+    sourceHandle: WorkflowOutputName;
+    target: WorkflowNodeId;
+    targetHandle: WorkflowInputName;
+    data: {
+      edge: WorkflowRuntimeEdge;
+    };
+  }[];
+};
+
+export type WorkflowRuntimeNode = {
+  id: WorkflowNodeId;
+  type: WorkflowNodeTypeName;
+  parentId?: WorkflowNodeId;
+  position: {
+    x: number;
+    y: number;
+    width: number;
+    height: number;
+    extent?: 'parent';
+  };
+  inputs: {
+    name: WorkflowInputName;
+    type: WorkflowValueType;
+    value?: WorkflowRuntimeValue;
+    edge?: WorkflowRuntimeEdge;
+  }[];
+  outputs: {
+    name: WorkflowOutputName;
+    type: WorkflowValueType;
+    value?: WorkflowRuntimeValue;
+    edges?: WorkflowRuntimeEdge[];
+  }[];
+  data: JsonObject;
+  mode?: `passthrough` | `disabled`;
+  executionState?: WorkflowRuntimeExecutionState;
+  graphErrors?: {
+    kind: `missing-type-definition`;
+  }[];
 };
 
 export type WorkflowRuntimeEdge = {
-    id: WorkflowEdgeId;
-    value?: WorkflowRuntimeValue;
-    source: {
+  id: WorkflowEdgeId;
+  value?: WorkflowRuntimeValue;
+  source:
+    | {
         node: WorkflowRuntimeNode;
         outputName: WorkflowOutputName;
         error?: undefined;
-    } | {
+      }
+    | {
         nodeId: WorkflowNodeId;
         outputName: WorkflowOutputName;
         error: `missing-source-node`;
-    };
-    target: {
-        node: WorkflowRuntimeNode;
-        inputName: WorkflowInputName;
-    };
-    graphErrors?: {
-        kind: `missing-source-node` | `missing-source-output` | `missing-target-input`;
-    }[];
+      };
+  target: {
+    node: WorkflowRuntimeNode;
+    inputName: WorkflowInputName;
+  };
+  graphErrors?: {
+    kind: `missing-source-node` | `missing-source-output` | `missing-target-input`;
+  }[];
 };
 
 export type WorkflowRuntimeValue<T = unknown> = {
-    /** valtio ref() */
-    data: T;
-    /** valtio ref() */
-    meta: {
-        type: WorkflowValueType;
-        source: {
-            nodeId: WorkflowNodeId;
-            outputName: WorkflowOutputName;
-            timestamp: WorkflowTimestamp;
-        };
+  /** valtio ref() */
+  data: T;
+  /** valtio ref() */
+  meta: {
+    type: WorkflowValueType;
+    source: {
+      nodeId: WorkflowNodeId;
+      outputName: WorkflowOutputName;
+      timestamp: WorkflowTimestamp;
     };
+  };
 };
 
 /** This whole store is a valtio object, just change it directly */
 export type WorkflowRuntimeStore = {
-    nodeTypes: Record<WorkflowNodeTypeName, WorkflowRuntimeNodeTypeDefinition>;
-    nodes: Record<WorkflowNodeId, WorkflowRuntimeNode>;
-    edges: Record<WorkflowEdgeId, WorkflowRuntimeEdge>;
-    actions: WorkflowRuntimeStoreActions;
+  nodeTypes: Record<WorkflowNodeTypeName, WorkflowRuntimeNodeTypeDefinition>;
+  nodes: Record<WorkflowNodeId, WorkflowRuntimeNode>;
+  edges: Record<WorkflowEdgeId, WorkflowRuntimeEdge>;
+  actions: WorkflowRuntimeStoreActions;
 };
 
 /** helpers to simplify some actions */
 export type WorkflowRuntimeStoreActions = {
-    createNodeType: (args: WorkflowRuntimeNodeTypeDefinition) => void;
-    deleteNodeType: (nodeType: WorkflowNodeTypeName) => void;
+  createNodeType: (args: WorkflowRuntimeNodeTypeDefinition) => void;
+  deleteNodeType: (nodeType: WorkflowNodeTypeName) => void;
 
-    createNode: (node: {
-        id: string;
-        type: WorkflowNodeTypeName;
-        parentId?: WorkflowNodeId;
-        position: {
-            x: number;
-            y: number,
-            width: number;
-            height: number;
-        };
-    }) => void;
-    deleteNode: (nodeId: WorkflowNodeId) => void;
+  createNode: (node: {
+    id: string;
+    type: WorkflowNodeTypeName;
+    parentId?: WorkflowNodeId;
+    position: {
+      x: number;
+      y: number;
+      width: number;
+      height: number;
+    };
+  }) => void;
+  deleteNode: (nodeId: WorkflowNodeId) => void;
 
-    createEdge: (edge: {
-        source: {
-            nodeId: WorkflowNodeId;
-            outputName: WorkflowOutputName;
-        };
-        target: {
-            nodeId: WorkflowNodeId;
-            inputName: WorkflowInputName;
-        };
-    }) => void;
-    deleteEdge: (edgeId: WorkflowEdgeId) => void;
+  createEdge: (edge: {
+    source: {
+      nodeId: WorkflowNodeId;
+      outputName: WorkflowOutputName;
+    };
+    target: {
+      nodeId: WorkflowNodeId;
+      inputName: WorkflowInputName;
+    };
+  }) => void;
+  deleteEdge: (edgeId: WorkflowEdgeId) => void;
 };
 
 export type WorkflowExecutionController = {
-    abortSignal: AbortSignal;
-    setProgress: (valueOrSetter: { progressRatio: number, message?: string } | ((prev: { progressRatio: number, message?: string }) => { progressRatio: number, message?: string })) => void;
+  abortSignal: AbortSignal;
+  setProgress: (
+    valueOrSetter:
+      | { progressRatio: number; message?: string }
+      | ((prev: { progressRatio: number; message?: string }) => {
+          progressRatio: number;
+          message?: string;
+        }),
+  ) => void;
 };
 
 export type WorkflowRuntimeNodeTypeDefinition = {
-    type: WorkflowNodeTypeName,
-    component: React.ComponentType,
-    inputs: {
-        name: WorkflowInputName;
-        type: WorkflowValueType;
-    }[];
-    outputs: {
-        name: WorkflowOutputName;
-        type: WorkflowValueType;
-    }[];
-    execute: (args: {
-        inputs: Record<string, unknown>,
-        data: JsonObject,
-        controller: WorkflowExecutionController,
-        node: WorkflowRuntimeNode,
-        store: WorkflowRuntimeStore,
-    }) => Promise<{ outputs: Record<string, unknown>, data?: JsonObject }>;
-}
+  type: WorkflowNodeTypeName;
+  component: React.ComponentType;
+  inputs: {
+    name: WorkflowInputName;
+    type: WorkflowValueType;
+  }[];
+  outputs: {
+    name: WorkflowOutputName;
+    type: WorkflowValueType;
+  }[];
+  execute: (args: {
+    inputs: Record<string, unknown>;
+    data: JsonObject;
+    controller: WorkflowExecutionController;
+    node: WorkflowRuntimeNode;
+    store: WorkflowRuntimeStore;
+  }) => Promise<{ outputs: Record<string, unknown>; data?: JsonObject }>;
+};
 
 export type WorkflowRuntimeExecutionState = {
-    status: `initial` | `running` | `success` | `error` | `aborted`;
-    startTimestamp?: WorkflowTimestamp;
-    endTimestamp?: WorkflowTimestamp;
-    progressRatio?: number;
-    progressMessage?: string;
-    errorMessage?: string;
+  status: `initial` | `running` | `success` | `error` | `aborted`;
+  startTimestamp?: WorkflowTimestamp;
+  endTimestamp?: WorkflowTimestamp;
+  progressRatio?: number;
+  progressMessage?: string;
+  errorMessage?: string;
 
-    /** Completed execution states */
-    history: {
-        status: `success` | `error` | `aborted`;
-        startTimestamp: WorkflowTimestamp;
-        endTimestamp: WorkflowTimestamp;
-        errorMessage?: string;
-    }[];
-}
+  /** Completed execution states */
+  history: {
+    status: `success` | `error` | `aborted`;
+    startTimestamp: WorkflowTimestamp;
+    endTimestamp: WorkflowTimestamp;
+    errorMessage?: string;
+  }[];
+};
 
 export type WorkflowRuntimeEngine = {
-    setup: (store: WorkflowRuntimeStore) => void;
-    /** start running the nodes, based on the engines scheduling logic */
-    start: () => void;
-    /** stop running the nodes, optionally abort current node executions */
-    stop: (args: { shouldAbort: boolean }) => void;
-    /** manually trigger a node to execute */
-    queueNode: (nodeId: WorkflowNodeId) => void;
+  setup: (store: WorkflowRuntimeStore) => void;
+  /** start running the nodes, based on the engines scheduling logic */
+  start: () => void;
+  /** stop running the nodes, optionally abort current node executions */
+  stop: (args: { shouldAbort: boolean }) => void;
+  /** manually trigger a node to execute */
+  queueNode: (nodeId: WorkflowNodeId) => void;
 };
